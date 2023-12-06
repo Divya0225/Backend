12b99cf2c264b0ef1d53c3e670ad8bf186b71f2c
divya <dasta1@unh.newhaven.edu>
2023-12-05 23:11:54 -0500

    first commit

---
+++ connection/mongoconnection.js
@@ -0,0 +1,6 @@
+module.exports = {
+    connectionurl: 'mongodb+srv://divya:Bhumi2325@cluster0.4spl1s3.mongodb.net/?retryWrites=true&w=majority',
+    database: 'clothingdata',
+    collection: 'clothingcollection'
+  };
+  
